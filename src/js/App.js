3edce32e122a61cb483a737238dd20a0232a3436
Dylan <dylan_krajnc@tutanota.com>
2023-03-21 18:30:12 +0200

    - Additions to BrowserWindows' webPreferences - Creation of `preload.js` file - Addition of SonarLint to the project

---
+++ src/js/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function App() {
+  const title = "Hello World Update";
+  const enhancedTitle = title + ' - React App!';
+
+  return (
+    <h1>{enhancedTitle}</h1>
+  )
+}
